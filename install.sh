12761371efc8c68926941f916fd3ff2398e100f6
zdolic <zdolic@gmail.com>
2018-02-06 08:23:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 rm ./install.log 2>/dev/null 1>&2
 
+dpkg-query -s wiringpi >> install.log
+dpkg-query -W --showformat='${Status}\n' wiringpi
+
 echo "Building node-wiring-pi ... "
 node-gyp clean 2>&1 | tee -a ./install.log
 node-gyp configure build 2>&1 | tee -a ./install.log
